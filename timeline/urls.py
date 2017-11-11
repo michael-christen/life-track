9f55fb5219d134d9d691883f909271532ed3afb2
michael-christen <mchristen96@gmail.com>
2017-11-10 19:38:57 -0800

    Add schema

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Timeline API')
 
 from . import views
 
@@ -10,6 +13,7 @@ router.register(r'groups', views.GroupViewSet)
 router.register(r'events', views.EventViewSet)
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
